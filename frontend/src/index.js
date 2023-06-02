969ec4dbdd7b662818c998ee57c12791048554a7
Dema Amano <demaamano@gmail.com>
2023-06-02 20:23:39 +0300

    frontend additional customization

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import CreateSubaccount from "./Account";
@@ -8,8 +9,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter> 
     <App />
-   {/* <CreateSubaccount/> */}
+    </BrowserRouter>
+    
+   
   </React.StrictMode>
 );
 
